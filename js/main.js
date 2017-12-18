c95beb209a445674d657b8f9dc502a692c7e890a
Jackson Doherty <jgdoherty23@gmail.com>
2017-12-17 17:50:41 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,92 @@
+console.log("debug: main");
+
+// FPS
+var clock;
+var deltaTime;
+
+// Scene
+var container;
+var renderer, scene, camera;
+var sphere;
+var ambientLight, pointLight;
+
+init();
+animate();
+
+function init()
+{
+	console.log("debug: init");
+
+	// clock
+	clock = new THREE.Clock();
+
+	// container
+	container = document.createElement('div');
+	document.body.appendChild(container);
+
+	// renderer
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	container.appendChild(renderer.domElement);
+
+	// scene
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color('white');
+
+	// camera
+	var fov = 50;
+	var aspect = window.innerWidth/window.innerHeight;
+	var near = 1;
+	var far = 100000;
+	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.z = -5;
+	camera.lookAt(scene.position);
+
+	// sphere
+	var sphereRadius = 1;
+	var sphereWidthSegments = 64;
+	var sphereHeightSegments = 64;
+	var sphereGeometry = new THREE.SphereGeometry(
+		sphereRadius,
+		sphereWidthSegments,
+		sphereHeightSegments
+	);
+	var sphereMaterial = new THREE.MeshStandardMaterial();
+	sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+	scene.add(sphere);
+
+	// ambient light
+	ambientLight = new THREE.AmbientLight(0xffffff);
+	scene.add(ambientLight);
+
+	// point light
+	pointLight = new THREE.PointLight(0xffffff, 0.5);
+	scene.add(pointLight);
+	pointLight.position.set(10, 15, -10);
+	scene.add(pointLight);
+
+	// listeners
+	window.addEventListener('resize', onWindowResize, false);
+}
+
+function onWindowResize() 
+{
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight );
+}
+
+function render() 
+{
+	renderer.render(scene, camera);
+}
+
+function animate() 
+{
+	requestAnimationFrame(animate);
+	deltaTime = clock.getDelta();
+	render();
+}
